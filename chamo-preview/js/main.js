90dcd096ed448de5ec2a24825d7d24f35d08ca3e
Farah Alia <farahalia.pk@gmail.com>
2016-10-13 00:25:53 +0500

    updated

---
+++ chamo-preview/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+ "use strict";
+
+/*----------------------------
+ wow js active
+------------------------------ */
+ new WOW().init();
+
+/*--------------------------
+ scrollUp
+---------------------------- */	
+	$.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    }); 	
+
+
+})(jQuery); 
