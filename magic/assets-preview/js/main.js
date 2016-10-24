5bcb46d83a77af3548f5fab06db9a0977d311a8a
Farah Alia <farahalia.pk@gmail.com>
2016-10-24 13:55:20 +0500

    updated

---
+++ magic/assets-preview/js/main.js
@@ -0,0 +1,16 @@
+/*
+ * POM - Creative Coming Soon Template
+ * Build Date: June 2015
+ * Author: Madeon08
+ * Copyright (C) 2015 Madeon08
+ * This is a premium product available exclusively here : http://themeforest.net/user/Madeon08/portfolio
+ */
+
+$(window).load(function() {
+
+    /*
+    * Loading
+    */
+    $('.loading-part').fadeOut();
+
+});
